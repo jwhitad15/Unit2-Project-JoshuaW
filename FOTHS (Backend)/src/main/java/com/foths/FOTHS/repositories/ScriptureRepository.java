8563a0dc8ac6982f7e2aa432092354b3ffaa9f04
jwhitad15 <jwhite83193@gmail.com>
2025-08-05 15:19:38 -0500

    Delete FOTHS (Backend) directory

--- FOTHS (Backend)/src/main/java/com/foths/FOTHS/repositories/ScriptureRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foths.FOTHS.repositories;
-
-import com.foths.FOTHS.models.Scripture;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScriptureRepository extends JpaRepository<Scripture, Integer> {
-}
--- FOTHS (Backend)/src/main/java/com/foths/FOTHS/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foths.FOTHS.repositories;
-
-import com.foths.FOTHS.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
