db5adbb12c9b763d95c4b068cee82c43a5e489aa
Joshua <jwhite83193@gmail.com>
2025-11-22 14:31:51 -0600

    starting over...again

---
+++ FOTHS [backend]/src/main/java/com/foths/application/controllers/ExampleController.java
@@ -0,0 +1,13 @@
+package com.foths.application.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
--- FOTHS [backend]/src/main/java/com/foths/application/controllers/UserController.java
+++ FOTHS [backend]/src/main/java/com/foths/application/controllers/UserController.java
@@ -1,100 +1,113 @@
-//Controller for managing user-related operations
-
-// It provides endpoints to create, read, update, and delete users in the application.
-// The controller uses the UserRepository to interact with the database and perform CRUD operations on User entities
-// It also handles HTTP requests and responses, returning appropriate status codes and messages based on the operation
-// performed.
-// The UserController class is annotated with @RestController to indicate that it is a Spring MVC controller
-// and @RequestMapping to specify the base URL for all endpoints in this controller.
-
-package com.foths.application.controllers;
-
-import com.foths.application.models.User;
-import com.foths.application.models.dto.LoginRequest;
-import com.foths.application.repositories.UserRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
-import java.util.List;
-
-
-@RestController
-@RequestMapping("/users")
-public class UserController {
-
-    private final UserRepository userRepository;
-
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @GetMapping("")
-    public ResponseEntity<?> getUsers() {
-        List<User> allUsers = userRepository.findAll();
-        return ResponseEntity.ok(allUsers);
+////Controller for managing user-related operations
+//
+//// It provides endpoints to create, read, update, and delete users in the application.
+//// The controller uses the UserRepository to interact with the database and perform CRUD operations on User entities
+//// It also handles HTTP requests and responses, returning appropriate status codes and messages based on the operation
+//// performed.
+//// The UserController class is annotated with @RestController to indicate that it is a Spring MVC controller
+//// and @RequestMapping to specify the base URL for all endpoints in this controller.
+//
+//package com.foths.application.controllers;
+//
+//import com.foths.application.models.User;
+//import com.foths.application.models.dto.LoginRequest;
+//import com.foths.application.repositories.UserRepository;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Objects;
+//
+//
+//@RestController
+//@RequestMapping("/users")
+//public class UserController {
+//
+//    private final UserRepository userRepository;
+//
+//    public UserController(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @GetMapping("")
+//    public ResponseEntity<?> getUsers() {
+//        List<User> allUsers = userRepository.findAll();
+//        return ResponseEntity.ok(allUsers);
 //        return new ResponseEntity<>(allUsers, HttpStatus.OK); // 200
-    }
-
-    @GetMapping(value="/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getUserById(@PathVariable(value="userId") int userId) {
-        User currentUser = (User) userRepository.findById(userId).orElse(null);
-        if (currentUser != null) {
-            return new ResponseEntity<>(currentUser, HttpStatus.OK); // 200
-        } else {
-            String response = STR."User with ID of \{userId} not found.";
-            return new ResponseEntity<>(Collections.singletonMap("response", response), HttpStatus.NOT_FOUND); // 404
-        }
-    }
-
-    @PostMapping("/create")
-    public ResponseEntity<?> createNewUser(@RequestBody User user) {
-        userRepository.save(user);
-        return new ResponseEntity<>(user, HttpStatus.CREATED); // 201
-    }
-
-    @PutMapping("/update/{userId}")
-    public ResponseEntity<?> updateUser(@PathVariable(value="userId") int userId, @RequestBody User user) {
-        User currentUser = (User) userRepository.findById(userId).orElse(null);
-        if (currentUser != null) {
-            currentUser.setFirstName(user.getFirstName());
-            currentUser.setLastName(user.getLastName());
-            currentUser.setEmail(user.getEmail());
-            currentUser.setUsername(user.getUsername());
-            currentUser.setPassword(user.getPassword());
-
-            userRepository.save(currentUser);
-            return new ResponseEntity<>(currentUser, HttpStatus.OK); // 200
-        } else {
-            String response = STR."User with ID of \{userId} not found.";
-            return new ResponseEntity<>(Collections.singletonMap("response", response), HttpStatus.NOT_FOUND); // 404
-        }
-    }
-
-    @DeleteMapping(value="/delete/{userId}", produces=MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> deleteById(@PathVariable(value="userId") int userId) {
-        User currentUser = (User) userRepository.findById(userId).orElse(null);
-        if (currentUser != null) {
-            userRepository.deleteById(userId);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204
-        } else {
-            String response = STR."User with ID of \{userId} not found.";
-            return new ResponseEntity<>(Collections.singletonMap("response", response), HttpStatus.NOT_FOUND); // 404
-        }
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
-        System.out.println("Login endpoint hit");
-        User user = userRepository.findByUsername(loginRequest.getUsername());
-        if (user != null && user.getPassword().equals(loginRequest.getPassword())) {
-            return ResponseEntity.ok(user);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(Collections.singletonMap("error", "Invalid username or password"));
-        }
-    }
-
-}
+//    }
+//
+//    @GetMapping(value="/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> getUserById(@PathVariable(value="userId") int userId) {
+//        User currentUser = (User) userRepository.findById(userId).orElse(null);
+//        if (currentUser != null) {
+//            return new ResponseEntity<>(currentUser, HttpStatus.OK); // 200
+//        } else {
+//            String response = STR."User with ID of \{userId} not found.";
+//            return new ResponseEntity<>(Collections.singletonMap("response", response), HttpStatus.NOT_FOUND); // 404
+//        }
+//    }
+//
+//    @PostMapping("/create")
+//    public ResponseEntity<?> createNewUser(@RequestBody User user) {
+//        userRepository.save(user);
+//        return new ResponseEntity<>(user, HttpStatus.CREATED); // 201
+//    }
+//
+//    @PutMapping("/update/{userId}")
+//    public ResponseEntity<?> updateUser(@PathVariable(value="userId") int userId, @RequestBody User user) {
+//        User currentUser = (User) userRepository.findById(userId).orElse(null);
+//        if (currentUser != null) {
+//            currentUser.setFirstName(user.getFirstName());
+//            currentUser.setLastName(user.getLastName());
+//            currentUser.setEmail(user.getEmail());
+//            currentUser.setUsername(user.getUsername());
+//            currentUser.setPassword(user.getPassword());
+//
+//            userRepository.save(currentUser);
+//            return new ResponseEntity<>(currentUser, HttpStatus.OK); // 200
+//        } else {
+//            String response = STR."User with ID of \{userId} not found.";
+//            return new ResponseEntity<>(Collections.singletonMap("response", response), HttpStatus.NOT_FOUND); // 404
+//        }
+//    }
+//
+//    @DeleteMapping(value="/delete/{userId}", produces=MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> deleteById(@PathVariable(value="userId") int userId) {
+//        User currentUser = (User) userRepository.findById(userId).orElse(null);
+//        if (currentUser != null) {
+//            userRepository.deleteById(userId);
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204
+//        } else {
+//            String response = STR."User with ID of \{userId} not found.";
+//            return new ResponseEntity<>(Collections.singletonMap("response", response), HttpStatus.NOT_FOUND); // 404
+//        }
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+//        System.out.println("Login endpoint hit");
+//        User user = userRepository.findByUsername(loginRequest.getUsername());
+//        if (user != null && user.getPassword().equals(loginRequest.getPassword())) {
+//            return ResponseEntity.ok(user);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                    .body(Collections.singletonMap("error", "Invalid username or password"));
+//        }
+//    }
+//
+//    // Renamed to avoid colliding with application login page (/login)
+//    @PostMapping("/authenticate")
+//    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
+//        User user = userRepository.findByUsername(loginRequest.getUsername());
+//        if (user != null && Objects.equals(user.getPassword(), loginRequest.getPassword())) {
+//            return ResponseEntity.ok(user);
+//        } else {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                    .body(Collections.singletonMap("error", "Invalid username or password"));
+//        }
+//    }
+//
+//}
