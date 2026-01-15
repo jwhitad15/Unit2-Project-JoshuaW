23fd8ac09f6a554c7e8819c8e9bd6c41d4e60790
Joshua <jwhite83193@gmail.com>
2026-01-14 18:14:25 -0600

    CLEANUP COMMIT: Committing Migration Changes for Live Deployment

---
+++ Related Files/sql-queries/Data Exports/Category_Fruit.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: SELECT * FROM fruit_category
+LIMIT 0, 1000
+
+-- Date: 2026-01-13 21:00
+*/
+INSERT INTO `` (`id`,`fruit`) VALUES (1,'Faith');
+INSERT INTO `` (`id`,`fruit`) VALUES (2,'Love');
+INSERT INTO `` (`id`,`fruit`) VALUES (3,'Joy');
+INSERT INTO `` (`id`,`fruit`) VALUES (4,'Peace');
+INSERT INTO `` (`id`,`fruit`) VALUES (5,'Temperance');
+INSERT INTO `` (`id`,`fruit`) VALUES (6,'Goodness');
+INSERT INTO `` (`id`,`fruit`) VALUES (7,'Gentleness');
+INSERT INTO `` (`id`,`fruit`) VALUES (8,'Meekness');
+INSERT INTO `` (`id`,`fruit`) VALUES (9,'Longsuffering');
