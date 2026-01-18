3ddcaae7a3d21702efbbbdcc29c5ac22a2f31575
Joshua <jwhite83193@gmail.com>
2026-01-17 20:28:50 -0600

    DEPLOYMENT: Connecting frontend with Railway. Added railway.json file.

---
+++ foths-backend/src/main/java/com/foths/application/dto/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.foths.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    public LoginRequest() {}
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+
+
+}
